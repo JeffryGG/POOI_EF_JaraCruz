9489047fd7ff8586653e41fb66e9d9f4f3947027
JeffryGG <Jeffry.jj.c@gmail.com>
2024-07-07 09:20:11 -0500

    Se creo la vista curso

---
+++ ProyFrontEnd/Pages/Cursos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyFrontEnd.Pages
+{
+    public class CursosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
